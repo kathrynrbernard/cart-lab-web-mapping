6fd3759ca87ebf607d8a347512b56d51d11fc5c0
Kathryn Bernard <kathrynrbernard@gmail.com>
2023-02-01 12:09:48 -0600

    proportionally sized circles

--- unit-2/Chapter05/js/main.js
+++ unit-2/Chapter05/js/main.js
@@ -1,15 +1,13 @@
-// Data from: https://geo.btaa.org/catalog/B540FDA4-3D6D-41C7-B01E-98C1C9F6B00E
-// Monona Transit and Middleton Trolley stops
+//declare map variable globally so all functions have access
+var map;
+var minValue;
 
-//declare map var in global scope
-let map;
-
-//function to instantiate the Leaflet map
+//step 1 create map
 function createMap() {
     //create the map
     map = L.map('map', {
-        center: [43.0747, -89.3841],
-        zoom: 13,
+        center: [0, 0],
+        zoom: 2,
     });
 
     //add OSM base tilelayer
@@ -19,82 +17,80 @@ function createMap() {
     }).addTo(map);
 
     //call getData function
-    getData();
+    getData(map);
 }
 
-const props = ['LOCATION', 'STOP_ID', 'SYSTEM'];
-function popupContent(feature, layer) {
-    let popupContent = '';
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (let property in props) {
-            let prop_display = props[property],
-                val_display = feature.properties[props[property]];
-            props[property] === 'STOP_ID'
-                ? (prop_display = 'STOP ID')
-                : (prop_display = prop_display);
-            if (props[property] === 'SYSTEM') {
-                if (
-                    feature.properties['SYSTEM'] === 'MON' ||
-                    feature.properties['SYSTEM'] === 'MT'
-                ) {
-                    val_display = 'Monona Trolley';
-                } else if (feature.properties['SYSTEM'] === 'MIDTROL') {
-                    val_display = 'Middleton Trolley';
-                }
-            }
-            popupContent +=
-                '<p><b>' + prop_display + '</b>: ' + val_display + '</p>';
+function calculateMinValue(data) {
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for (var city of data.features) {
+        //loop through each year
+        for (var year = 1985; year <= 2015; year += 5) {
+            //get population for current year
+            var value = city.properties['Pop_' + String(year)];
+            //add value to array
+            allValues.push(value);
         }
-        layer.bindPopup(popupContent);
     }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues);
+
+    return minValue;
 }
 
-//function to retrieve the data and place it on the map
-function getData() {
-    //load the data
-    fetch('data/Madison_City_Suburban_Transit_Stops_2020.geojson')
-        .then((response) => response.json())
-        .then(callback);
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue / minValue, 0.5715) * minRadius;
+
+    return radius;
 }
 
-let mononaMarker = {
-    radius: 8,
-    fillColor: '#ff7800',
-    color: '#000',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-};
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data) {
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    var attribute = 'Pop_2015';
 
-let middletonMarker = {
-    radius: 8,
-    fillColor: '#748d69',
-    color: '#000',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-};
+    //create marker options
+    var geojsonMarkerOptions = {
+        fillColor: '#ff7800',
+        color: '#fff',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        radius: 8,
+    };
 
-function getMarker(feature) {
-    switch (feature.properties.SYSTEM) {
-        case 'MON':
-            console.log('hi');
-            return mononaMarker;
-        case 'MT':
-            return mononaMarker;
-        case 'MIDTROL':
-            return middletonMarker;
-    }
-}
-
-function callback(response) {
-    L.geoJson(response, {
-        onEachFeature: popupContent,
+    L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, getMarker(feature));
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //Step 6: Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
         },
     }).addTo(map);
+}
+
+//Step 2: Import GeoJSON data
+function getData() {
+    //load the data
+    fetch('data/MegaCities.geojson')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', createMap);
