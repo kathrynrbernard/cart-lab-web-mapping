854be366d3538c9e477d3f87775eb5f872a74a4e
Kathryn Bernard <kathrynrbernard@gmail.com>
2023-02-01 12:04:17 -0600

    Add back files removed by mistake

--- unit-2/Chapter04/js/main.js
+++ unit-2/Chapter04/js/main.js
@@ -1,15 +1,13 @@
-// Data from: https://geo.btaa.org/catalog/B540FDA4-3D6D-41C7-B01E-98C1C9F6B00E
-// Monona Transit and Middleton Trolley stops
+/* Map of GeoJSON data from MegaCities.geojson */
 
 //declare map var in global scope
-let map;
-
+var map;
 //function to instantiate the Leaflet map
 function createMap() {
     //create the map
     map = L.map('map', {
-        center: [43.0747, -89.3841],
-        zoom: 13,
+        center: [20, 0],
+        zoom: 2,
     });
 
     //add OSM base tilelayer
@@ -22,29 +20,14 @@ function createMap() {
     getData();
 }
 
-const props = ['LOCATION', 'STOP_ID', 'SYSTEM'];
-function popupContent(feature, layer) {
-    let popupContent = '';
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = '';
     if (feature.properties) {
         //loop to add feature property names and values to html string
-        for (let property in props) {
-            let prop_display = props[property],
-                val_display = feature.properties[props[property]];
-            props[property] === 'STOP_ID'
-                ? (prop_display = 'STOP ID')
-                : (prop_display = prop_display);
-            if (props[property] === 'SYSTEM') {
-                if (
-                    feature.properties['SYSTEM'] === 'MON' ||
-                    feature.properties['SYSTEM'] === 'MT'
-                ) {
-                    val_display = 'Monona Trolley';
-                } else if (feature.properties['SYSTEM'] === 'MIDTROL') {
-                    val_display = 'Middleton Trolley';
-                }
-            }
+        for (var property in feature.properties) {
             popupContent +=
-                '<p><b>' + prop_display + '</b>: ' + val_display + '</p>';
+                '<p>' + property + ': ' + feature.properties[property] + '</p>';
         }
         layer.bindPopup(popupContent);
     }
@@ -53,47 +36,29 @@ function popupContent(feature, layer) {
 //function to retrieve the data and place it on the map
 function getData() {
     //load the data
-    fetch('data/Madison_City_Suburban_Transit_Stops_2020.geojson')
+    fetch('data/MegaCities.geojson')
         .then((response) => response.json())
-        .then(callback);
-}
-
-let mononaMarker = {
-    radius: 8,
-    fillColor: '#ff7800',
-    color: '#000',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-};
-
-let middletonMarker = {
-    radius: 8,
-    fillColor: '#748d69',
-    color: '#000',
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-};
-
-function getMarker(feature) {
-    switch (feature.properties.SYSTEM) {
-        case 'MON':
-            console.log('hi');
-            return mononaMarker;
-        case 'MT':
-            return mononaMarker;
-        case 'MIDTROL':
-            return middletonMarker;
-    }
+        .then(callback); // why not callback(response)?
+    // Because callback is getting called on the return of the last .then so it's response.callback? Then why is response included in () below?
 }
 
 function callback(response) {
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: '#ff7800',
+        color: '#000',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+    };
+    //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(response, {
-        onEachFeature: popupContent,
         pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, getMarker(feature));
+            // what is feature here?
+            // The GeoJSON point feature: https://leafletjs.com/reference.html#geojson-pointtolayer
+            return L.circleMarker(latlng, geojsonMarkerOptions);
         },
+        onEachFeature: onEachFeature,
     }).addTo(map);
 }
 
